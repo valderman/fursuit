c62ef08345b9881e2ddc820bf075f77c8ff817b4
Anton Ekblad <anton@ekblad.cc>
2012-07-04 11:05:56 +0200

    Remove the build-for-Haste part from Setup.hs; it's moved to haste-boot.

--- Setup.hs
+++ Setup.hs
@@ -1,28 +1,3 @@
 import Distribution.Simple
-import System.Exit (ExitCode (..))
-import System.Process (waitForProcess, runProcess)
-import System.Directory (getAppUserDataDirectory)
 
-main = defaultMainWithHooks $ simpleUserHooks {postInst = buildForHaste}
-
--- Attempt to install library for use with haste-compiler
-buildForHaste _ _ _ _ = do
-  cabalDir <- getAppUserDataDirectory "cabal"
-  let localHastec = cabalDir ++ "/bin/hastec"
-  buildWith ["hastec", localHastec]
-
-buildWith (hastec:hs) = do
-  build <- runProcess hastec
-                      ["-O2", "-Wall", "--libinstall", "FRP.Fursuit",
-                       "FRP.Fursuit.Async"]
-                      (Just "./src")
-                      Nothing
-                      Nothing
-                      Nothing
-                      Nothing
-  res <- waitForProcess build
-  case res of
-    ExitFailure _ -> buildWith hs
-    _             -> return ()
-buildWith _ =
-  return ()
+main = defaultMain
